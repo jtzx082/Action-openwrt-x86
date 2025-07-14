0b289b02a3454115536a4a794dea8c28d150efbd
jtzx082 <yujq804@gmail.com>
2025-07-14 16:09:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,12 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# 修复erofs编译依赖
+sed -i 's/DEPENDS:= /DEPENDS:= +liblz4 +libuuid +tcping +geoview +LingTiGameAcc /' package/utils/erofs-utils/Makefile
+
+# 若仍失败，回退到稳定版本
+#echo "src-git erofs https://github.com/openwrt/openwrt.git" >> feeds.conf.default
+
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
