bdd5098e523b708e0ebeaac37122f94add51715c
jtzx082 <yujq804@gmail.com>
2025-07-14 16:23:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 修复erofs编译依赖
-sed -i 's/DEPENDS:= /DEPENDS:= +liblz4 +libuuid +tcping +geoview +LingTiGameAcc /' package/utils/erofs-utils/Makefile
+sed -i 's/DEPENDS:= /DEPENDS:= +liblz4 +libuuid /' package/utils/erofs-utils/Makefile
 
 # 若仍失败，回退到稳定版本
 #echo "src-git erofs https://github.com/openwrt/openwrt.git" >> feeds.conf.default
